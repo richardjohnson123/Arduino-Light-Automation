6edcde977c59a39f19faf838ffff291863534e97
Richard Johnson <richardjohnson3@cmail.carleton.ca>
2024-09-24 18:26:11 -0400

    Adding Changes

--- wokwi Simulation/sketch/sketch.ino
+++ wokwi Simulation/sketch/sketch.ino
@@ -1,10 +1,10 @@
 #include<Servo.h>
 
 Servo servo;
-int led = 12;
-int ldr = A0;
-int potpin = A1;  // analog pin used to connect the potentiometer
-int intensity;    // variable to read the value from the analog pin
+const int led = 12;
+const int ldr = A0;
+const int potpin = A1;  // analog pin used to connect the potentiometer
+int intensity = 1;    // variable to read the value from the analog pin
 int count;
 
 // LDR Characteristics
@@ -17,60 +17,71 @@ void setup() {
 
   pinMode(ldr, INPUT);
   pinMode(led, OUTPUT);
+  pinMode(5, INPUT_PULLUP);
   servo.attach(9);
   servo.write(0);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
-  int analogValue = analogRead(A0);
-  float voltage = analogValue / 1024. * 5;
-  float resistance = 2000 * voltage / (1 - voltage / 5);
-  float lux = pow(RL10 * 1e3 * pow(10, GAMMA) / resistance, (1 / GAMMA));
+  if (digitalRead((5)) == LOW) {
 
-  intensity = analogRead(potpin);            // reads the value of the potentiometer (value between 0 and 1023)
-  intensity = map(intensity, 0, 1023, 0, 100);
+    intensity = analogRead(potpin);            // reads the value of the potentiometer (value between 0 and 1023)
+    intensity = map(intensity, 0, 1023, 0, 100);
+  }
+
+  if (digitalRead((5)) == HIGH) {
+
+    int analogValue = analogRead(A0);
+    float voltage = analogValue / 1024. * 5;
+    float resistance = 2000 * voltage / (1 - voltage / 5);
+    float lux = pow(RL10 * 1e3 * pow(10, GAMMA) / resistance, (1 / GAMMA));
+
+    Serial.println(resistance);
+
+    if (count >= 2) {
+
+      //Error: Something has gone wrong
+      digitalWrite(led, HIGH);
+    }
+
+    else {
+
+      digitalWrite(led, LOW);
+    }
+
+    if (lux > intensity) {
+
+      Serial.println("Light!");
+
+      if (count <= 1) {
+
+        count = 0;
+      }
+    }
+
+    else {
+
+      //Serial.println("Dark!");
+
+      if (count < 2) {
+
+        delay(2000);
+        servo.write(28);
+        delay(4000);
+        servo.write(0);
+        count++;
+      }
+    }
+
+    //Serial.print("Lux: ");
+    //Serial.print(lux);
+    //Serial.println("          ");
+
+    delay(100);
+  }
 
   Serial.print("Intensity: ");
   Serial.println(intensity);
-  if(count >= 2){
-
-    digitalWrite(led, HIGH);
-  }
-
-  else{
-
-    digitalWrite(led, LOW);
-  }
-
-  if (lux > intensity) {
-
-    Serial.println("Light!");
-
-    if(count <= 1){
-
-      count = 0;
-    }
-  } 
-  
-  else {
-
-    Serial.println("Dark!");
-
-    if(count < 2){
-
-      delay(2000);
-      servo.write(28);
-      delay(4000);
-      servo.write(0);
-      count++;
-    }
-  }
-
-  Serial.print("Lux: ");
-  Serial.print(lux);
-  Serial.println("          ");
-
   delay(100);
 }
